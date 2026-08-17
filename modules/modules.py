64d27f1d2b80450c2f5b220a554768a2c1a4dbf4
Lydia <Lydmeghe@gmail.com>
2026-08-17 02:22:17 +0100

    modules

--- modules/modules.py
+++ modules/modules.py
@@ -1,12 +1,19 @@
+#-----main.py------
+
 #module = a file containing code you want to include in your program 
 # use 'import' to include a module (built-in or your own)
 # useful to break up a large program reusable separate files 
 
 
 
+#print(help("modules"))
+#print(help("math"))
+
 #import math 
 #import math as m 
 #from math import e 
+
+
 import examplemod
 """a,b,c,d,e = 1,2,3,4,5
 
