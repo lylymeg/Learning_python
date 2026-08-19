fd126c84eb29459e59941b0420332c6fd8bb04cd
Lydia <Lydmeghe@gmail.com>
2026-08-19 23:05:19 +0100

    Hangman game

---
+++ wordslist.py
@@ -0,0 +1,26 @@
+#words for hangman game 
+
+
+words = (
+    "alligator", "alpaca", "ant", "anteater", "antelope", "ape", "armadillo", "donkey",
+    "baboon", "badger", "barracuda", "bat", "bear", "beaver", "bee", "bison", "boar",
+    "buffalo", "butterfly", "camel", "capybara", "caribou", "cat", "caterpillar",
+    "cheetah", "chicken", "chimpanzee", "chinchilla", "cobra", "cockroach", "cod",
+    "coyote", "crab", "crane", "crocodile", "crow", "deer", "dinosaur", "dog",
+    "dolphin", "dove", "dragonfly", "duck", "eagle", "eel", "elephant", "elk",
+    "falcon", "ferret", "finch", "flamingo", "fly", "fox", "frog", "gazelle",
+    "gerbil", "giraffe", "goat", "goldfish", "goose", "gorilla", "grasshopper",
+    "hamster", "hedgehog", "heron", "herring", "hippopotamus", "hornet", "horse",
+    "hummingbird", "hyena", "ibex", "iguana", "jackal", "jaguar", "jellyfish",
+    "kangaroo", "koala", "komodo dragon", "lemur", "leopard", "lion", "lizard",
+    "llama", "lobster", "lynx", "manatee", "mantis", "meerkat", "mink", "mole",
+    "mongoose", "monkey", "moose", "mosquito", "mouse", "mule", "narwhal", "newt",
+    "nightingale", "octopus", "okapi", "opossum", "ostrich", "otter", "owl", "ox",
+    "oyster", "panda", "panther", "parrot", "pelican", "penguin", "pheasant", "pig",
+    "pigeon", "polar bear", "porcupine", "porpoise", "quail", "rabbit", "raccoon",
+    "ram", "rat", "raven", "reindeer", "rhinoceros", "salamander", "salmon", "sardine",
+    "scorpion", "seahorse", "seal", "shark", "sheep", "sloth", "snail", "snake",
+    "spider", "squid", "squirrel", "starfish", "stork", "swan", "tapir", "tarantula",
+    "tiger", "toad", "trout", "turkey", "turtle", "viper", "vulture", "walrus",
+    "wasp", "weasel", "whale", "wolf", "wolverine", "wombat", "woodpecker", "yak", "zebra"
+)
