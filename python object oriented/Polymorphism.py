3772ab107634147db480c99b12090129cf7bde81
Lydia <Lydmeghe@gmail.com>
2026-08-20 14:00:25 +0100

    Polymorphism

---
+++ python object oriented/Polymorphism.py
@@ -0,0 +1,48 @@
+#polymorphism = greek word that means to have many forms or faces
+# poly = many 
+# morphe = form
+
+#two ways to achieve polymorphism
+#1.inheritance = an object could be treated the same of the same type as a parent class
+#2."duck typing" = object must have necessary attributes/methods
+
+from abc import ABC,abstractmethod
+
+class Shape: 
+    @abstractmethod 
+    def area(self):
+        pass 
+
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius = radius
+
+    def area(self):
+               return 3.14 * self.radius**2
+
+class Square(Shape):
+    def __init__(self,side):
+            self.side = side
+    def area(self):
+           return self.side**2
+    
+
+class Triangle(Shape):
+   def __init__(self,base,height):
+            self.base = base
+            self.height = height
+
+   def area(self):
+     return self.base * self.height *0.5
+
+
+
+class Pizza(Circle):
+     def __init__(self,topping,radius):
+          super().__init__(radius)
+          self.topping = topping
+         
+
+shapes = [Circle(4),Square(5),Triangle(6,7),Pizza("pepperoni",15)]
+for shape in shapes:
+   print(f"{shape.area()} cm ² ") # ² = alt + 0178
